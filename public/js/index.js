d4bef09d84eada33020ae1d6cf3a97196e785c79
Ajay Yadav <ajayyadav4258@gmail.com>
2020-04-11 15:26:27 +0530

    Removed default room

--- public/js/index.js
+++ public/js/index.js
@@ -7,9 +7,6 @@ const $dropdown = document.querySelector('#dropdown')
 const dropdownTemplate = document.querySelector('#dropdown-template').innerHTML
 
     socket.emit('getActiveRooms', (activeRooms) => {
-        if (!activeRooms.includes('default')) {
-            activeRooms = activeRooms.concat('default')
-        }
         activeRooms.forEach((room) => {            
             const html = Mustache.render(dropdownTemplate, { room })
             $dropdown.insertAdjacentHTML('beforeend', html)
